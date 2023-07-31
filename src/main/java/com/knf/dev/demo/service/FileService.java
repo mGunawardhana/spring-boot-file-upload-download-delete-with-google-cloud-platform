547d9f16a894593e83353ece6b61386cee8c6eb5
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-07-31 07:47:55 +0530

    content update

--- src/main/java/com/knf/dev/demo/service/FileService.java
+++ src/main/java/com/knf/dev/demo/service/FileService.java
@@ -4,6 +4,9 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
+/**
+ * Developed by - mGunawadhana
+ * */
 public interface FileService {
     List<String> listOfFiles();
     ByteArrayResource downloadFile(String fileName);
